91116cd45f6643525c29a60237f3feb51ee265b5
Anush bhise <anushbhise29@gmail.com>
2026-03-02 13:36:00 +0530

    Console Application part 2

---
+++ ConsoleApp2/Module1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Student
+    Private name As String = "Anush"
+
+    Private Sub show()
+        Console.WriteLine("hello" & name)
+    End Sub
+    Public Sub detials()
+        show()
+    End Sub
+End Class
+Module Module1
+    Sub main()
+        Dim S As New Student()
+        'S.show()
+        S.detials()
+    End Sub
+
+End Module
