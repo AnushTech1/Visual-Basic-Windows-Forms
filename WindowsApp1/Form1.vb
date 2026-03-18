e3357b81fa75ee9a0e8c017a92cd9fb091cb47de
Anush Bhise <anushbhise29@gmail.com>
2026-03-18 12:51:47 +0530

    Windows Appliction 1

---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim age As Integer = InputBox("enter your age", "age Input")
+        MsgBox(age + 2)
+    End Sub
+End Class
