8187dc72ed960a2d92fb1e6824e0366a02128c95
Anush Bhise <anushbhise29@gmail.com>
2026-03-23 14:03:11 +0530

    Windows Forms Appliction

---
+++ WinFormsApp2/Form1.Designer.vb
@@ -0,0 +1,220 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label2 = New Label()
+        ComboBox1 = New ComboBox()
+        TextBox3 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Label3 = New Label()
+        Label4 = New Label()
+        TextBox4 = New TextBox()
+        DataGridView1 = New DataGridView()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 10F, FontStyle.Bold)
+        Label1.Location = New Point(231, 89)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(49, 19)
+        Label1.TabIndex = 0
+        Label1.Text = "Name"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(338, 81)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(136, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(338, 154)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(136, 23)
+        TextBox2.TabIndex = 2
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 10F, FontStyle.Bold)
+        Label2.Location = New Point(231, 162)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(45, 19)
+        Label2.TabIndex = 3
+        Label2.Text = "Email"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(592, 81)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(121, 23)
+        ComboBox1.TabIndex = 4
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(338, 226)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(136, 23)
+        TextBox3.TabIndex = 5
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button1.Location = New Point(182, 400)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(78, 38)
+        Button1.TabIndex = 6
+        Button1.Text = "Insert"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button2.Location = New Point(318, 400)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(78, 38)
+        Button2.TabIndex = 7
+        Button2.Text = "Update"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button3.Location = New Point(459, 400)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(78, 38)
+        Button3.TabIndex = 8
+        Button3.Text = "Delete"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button4.Location = New Point(608, 400)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(78, 38)
+        Button4.TabIndex = 9
+        Button4.Text = "Search"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Button5.Location = New Point(733, 400)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(78, 38)
+        Button5.TabIndex = 10
+        Button5.Text = "Load"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 10F, FontStyle.Bold)
+        Label3.Location = New Point(231, 226)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(58, 19)
+        Label3.TabIndex = 11
+        Label3.Text = "Gender"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 10F, FontStyle.Bold)
+        Label4.Location = New Point(231, 293)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(60, 19)
+        Label4.TabIndex = 12
+        Label4.Text = "Contact"
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(338, 290)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(136, 23)
+        TextBox4.TabIndex = 13
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(543, 163)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.Size = New Size(240, 150)
+        DataGridView1.TabIndex = 14
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(921, 520)
+        Controls.Add(DataGridView1)
+        Controls.Add(TextBox4)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox3)
+        Controls.Add(ComboBox1)
+        Controls.Add(Label2)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+
+End Class
---
+++ WinFormsApp2/Form1.vb
@@ -0,0 +1,95 @@
+﻿
+Imports System.Data.OleDb
+
+Public Class Form1
+    Dim con As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\anush\OneDrive\Documents\studentDB.accdb")
+    Dim cmd As OleDbCommand
+    Dim da As OleDbDataAdapter
+    Dim dt As DataTable
+
+    Private Sub button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            con.Open()
+            Dim query As String = "INSERT INTO Users (Name, Email, Gender, Contact) VALUES (?, ?, ?, ?)"
+            cmd = New OleDbCommand(query, con)
+            cmd.Parameters.AddWithValue("?", TextBox1.Text)
+            cmd.Parameters.AddWithValue("?", TextBox2.Text)
+            cmd.Parameters.AddWithValue("?", TextBox3.Text)
+            cmd.Parameters.AddWithValue("?", TextBox4.Text)
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Record Inserted Successfully")
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            con.Close()
+        End Try
+    End Sub
+
+    Private Sub button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            con.Open()
+            Dim query As String = "UPDATE Users SET Email=?, Gender=?, Contact=? WHERE Name=?"
+            cmd = New OleDbCommand(query, con)
+            cmd.Parameters.AddWithValue("?", TextBox2.Text)
+            cmd.Parameters.AddWithValue("?", TextBox3.Text)
+            cmd.Parameters.AddWithValue("?", TextBox4.Text)
+            cmd.Parameters.AddWithValue("?", TextBox1.Text)
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Record Updated Successfully")
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            con.Close()
+        End Try
+    End Sub
+
+    Private Sub button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            con.Open()
+            Dim query As String = "DELETE FROM Users WHERE Name=?"
+            cmd = New OleDbCommand(query, con)
+            cmd.Parameters.AddWithValue("?", TextBox1.Text)
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Record Deleted Successfully")
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            con.Close()
+        End Try
+    End Sub
+
+    Private Sub button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Try
+            con.Open()
+            Dim query As String = "SELECT * FROM Users WHERE Name=?"
+            cmd = New OleDbCommand(query, con)
+            cmd.Parameters.AddWithValue("?", TextBox1.Text)
+            Dim reader As OleDbDataReader = cmd.ExecuteReader()
+            If reader.Read() Then
+                TextBox2.Text = reader("Email").ToString()
+                TextBox3.Text = reader("Gender").ToString()
+                TextBox4.Text = reader("Contact").ToString()
+            Else
+                MessageBox.Show("No Record Found")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            con.Close()
+        End Try
+    End Sub
+
+    Private Sub button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Try
+            con.Open()
+            da = New OleDbDataAdapter("SELECT * FROM Users", con)
+            dt = New DataTable()
+            da.Fill(dt)
+            DataGridView1.DataSource = dt
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            con.Close()
+        End Try
+    End Sub
+End Class
