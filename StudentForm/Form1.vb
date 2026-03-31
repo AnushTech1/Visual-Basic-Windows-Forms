e6aa49779d6d33a057c9ad4f125baa341a9703be
Anush Bhise <anushbhise29@gmail.com>
2026-03-31 11:17:58 +0530

    Student Form

---
+++ StudentForm/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim age As Integer = Convert.ToInt32(TextBox2.Text())
+        If age >= 18 Then
+            TextBox3.Text = "Eligible"
+        Else
+            TextBox3.Text = "eneligible"
+        End If
+    End Sub
+End Class
---
+++ StudentForm/My Project/Login.vb
@@ -0,0 +1,18 @@
+﻿Public Class Login
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "Admin" Or TextBox2.Text = "1234567890" Then
+            Me.Hide()
+            Dim Dashboard As New Form2()
+            Dashboard.show()
+        Else
+            Dim result As MsgBoxResult = MsgBox("Invaild Login detials", MsgBoxStyle.OkCancel + MsgBoxStyle.Critical, "System Message")
+            If result = MsgBoxResult.Cancel Then
+                Application.Exit()
+
+            Else
+                TextBox1.Clear()
+                TextBox2.Clear()
+            End If
+        End If
+    End Sub
+End Class
