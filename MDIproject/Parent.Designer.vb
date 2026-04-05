ac7a6793eb57e624240658fb1fc6795b63da1b61
Anush Bhise <anushbhise29@gmail.com>
2026-04-05 20:09:05 +0530

    Mulit Document Interface Project

---
+++ MDIproject/Parent.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Parent
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        MenuStrip1 = New MenuStrip()
+        FileToolStripMenuItem = New ToolStripMenuItem()
+        NewToolStripMenuItem = New ToolStripMenuItem()
+        WindowsToolStripMenuItem = New ToolStripMenuItem()
+        CascadeToolStripMenuItem = New ToolStripMenuItem()
+        TileVerticalToolStripMenuItem = New ToolStripMenuItem()
+        TileHorizontalToolStripMenuItem = New ToolStripMenuItem()
+        MenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {FileToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(800, 24)
+        MenuStrip1.TabIndex = 0
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' FileToolStripMenuItem
+        ' 
+        FileToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {NewToolStripMenuItem})
+        FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        FileToolStripMenuItem.Size = New Size(37, 20)
+        FileToolStripMenuItem.Text = "File"
+        ' 
+        ' NewToolStripMenuItem
+        ' 
+        NewToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {WindowsToolStripMenuItem})
+        NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        NewToolStripMenuItem.Size = New Size(180, 22)
+        NewToolStripMenuItem.Text = "New"
+        ' 
+        ' WindowsToolStripMenuItem
+        ' 
+        WindowsToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {CascadeToolStripMenuItem, TileVerticalToolStripMenuItem, TileHorizontalToolStripMenuItem})
+        WindowsToolStripMenuItem.Name = "WindowsToolStripMenuItem"
+        WindowsToolStripMenuItem.Size = New Size(180, 22)
+        WindowsToolStripMenuItem.Text = "Windows"
+        ' 
+        ' CascadeToolStripMenuItem
+        ' 
+        CascadeToolStripMenuItem.Name = "CascadeToolStripMenuItem"
+        CascadeToolStripMenuItem.Size = New Size(180, 22)
+        CascadeToolStripMenuItem.Text = "Cascade"
+        ' 
+        ' TileVerticalToolStripMenuItem
+        ' 
+        TileVerticalToolStripMenuItem.Name = "TileVerticalToolStripMenuItem"
+        TileVerticalToolStripMenuItem.Size = New Size(180, 22)
+        TileVerticalToolStripMenuItem.Text = "Tile Vertical"
+        ' 
+        ' TileHorizontalToolStripMenuItem
+        ' 
+        TileHorizontalToolStripMenuItem.Name = "TileHorizontalToolStripMenuItem"
+        TileHorizontalToolStripMenuItem.Size = New Size(180, 22)
+        TileHorizontalToolStripMenuItem.Text = "Tile Horizontal"
+        ' 
+        ' Parent
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(MenuStrip1)
+        IsMdiContainer = True
+        MainMenuStrip = MenuStrip1
+        Name = "Parent"
+        Text = "Form1"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents WindowsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CascadeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TileVerticalToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TileHorizontalToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ MDIproject/Parent.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Diagnostics.Tracing
+
+Public Class Parent
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        Dim child As New ChildForm()
+        child.Parent = Me
+        child.Show()
+    End Sub
+
+    'Arrange windows - Cascade
+    Private Sub CascadeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CascadeToolStripMenuItem.Click
+        LayoutMdi(MdiLayout.Cascade)
+    End Sub
+
+
+    'Arrange windows - Tile Vertical
+    Private Sub TileVerticalToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TileVerticalToolStripMenuItem.Click
+        LayoutMdi(MdiLayout.TileVertical)
+    End Sub
+
+    'Arrange windows - Tile Horizontal
+    Private Sub TileHorizontalToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TileHorizontalToolStripMenuItem.Click
+        LayoutMdi(MdiLayout.TileHorizontal)
+    End Sub
+End Class
