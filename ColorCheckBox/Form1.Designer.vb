bf93212d225167020b898a0d6746980a93e5b64a
Anush Bhise <anushbhise29@gmail.com>
2026-04-20 10:58:23 +0530

    Color Check Box

---
+++ ColorCheckBox/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        CheckBox1 = New CheckBox()
+        CheckBox2 = New CheckBox()
+        CheckBox3 = New CheckBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(361, 31)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(71, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Color Name"
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(59, 87)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(57, 19)
+        CheckBox1.TabIndex = 1
+        CheckBox1.Text = "Green"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(59, 130)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(46, 19)
+        CheckBox2.TabIndex = 2
+        CheckBox2.Text = "Red"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox3
+        ' 
+        CheckBox3.AutoSize = True
+        CheckBox3.Location = New Point(59, 171)
+        CheckBox3.Name = "CheckBox3"
+        CheckBox3.Size = New Size(49, 19)
+        CheckBox3.TabIndex = 3
+        CheckBox3.Text = "Blue"
+        CheckBox3.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(CheckBox3)
+        Controls.Add(CheckBox2)
+        Controls.Add(CheckBox1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+
+End Class
---
+++ ColorCheckBox/Form1.vb
@@ -0,0 +1,42 @@
+﻿Public Class Form1
+    Dim lbl As New Label()
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lbl.AutoSize = True
+        lbl.Location = New Point(30%, 50%)
+        lbl.Visible = False
+        Me.Controls.Add(lbl)
+    End Sub
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+        If CheckBox1.Checked Then
+            lbl.Text = "This is green color"
+            lbl.ForeColor = Color.White
+            lbl.BackColor = Color.Green
+            lbl.Visible = True
+        Else
+            lbl.Visible = False
+        End If
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked Then
+            lbl.BackColor = Color.Red
+            lbl.ForeColor = Color.White
+            lbl.Text = "This is red color"
+            lbl.Visible = True
+        Else
+            lbl.Visible = False
+        End If
+    End Sub
+    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+        If CheckBox3.Checked Then
+            lbl.BackColor = Color.Blue
+            lbl.ForeColor = Color.White
+            lbl.Text = "This is blue color"
+            lbl.Visible = True
+        Else
+            lbl.Visible = False
+        End If
+    End Sub
+
+End Class
