4496db237d0928fdd26d7e4d41d82f5dd3d640d2
Anush Bhise <anushbhise29@gmail.com>
2026-04-20 10:54:58 +0530

    Small Projects

---
+++ Small_Projects/Basic_calculator.vb
@@ -0,0 +1,23 @@
+﻿
+Imports System.Reflection.Emit
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Basic_calculator
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim num1 As Integer = Convert.ToInt32(TextBox1.Text)
+            Dim num2 As Integer = Val(TextBox2.Text)
+            Dim result As Integer = num1 / num2
+            Label4.Text = result
+        Catch ex As DivideByZeroException
+            MessageBox.Show("error can divided by zero", "exception")
+        Catch ex As FormatException
+            MessageBox.Show("Error: place enter valid number", "Exceptio")
+        Catch ex As Exception
+            MessageBox.Show("General error:" & ex.Message, "Exception")
+        Finally
+            Label5.clear()
+            Label5.Text &= vbCrLf & "Process complete"
+        End Try
+    End Sub
+End Class
---
+++ Small_Projects/Grade_Finder.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Reflection.Emit
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Grade_Finder
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim H As Double, i As Double, M As Double, S As Double, SS As Double, total As Double, avg As Double
+        Try
+            H = CDbl(TextBox1.Text)
+            i = CDbl(TextBox2.Text)
+            M = CDbl(TextBox3.Text)
+            S = CDbl(TextBox4.Text)
+            SS = CDbl(TextBox5.Text)
+            'Check data
+
+            total = H + i + M + S + SS
+            avg = total / 5
+            If avg >= 75 Then
+                Label8.Text = "A"
+            ElseIf avg >= 60 Then
+                Label8.Text = "B"
+            ElseIf avg >= 50 Then
+                Label8.Text = "C"
+            ElseIf avg >= 40 Then
+                Label8.Text = "D"
+            Else
+                Label8.Text = "Fail"
+            End If
+        Catch ex As FormatException
+            MessageBox.Show("Please Enter a Valid Number!")
+        End Try
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Application.Exit()
+    End Sub
+End Class
